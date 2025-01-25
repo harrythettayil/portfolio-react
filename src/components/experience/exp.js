1a277d3d82b3d005935e02f4e7a6d6b228472583
Harry <t.a.harry@gmail.com>
2025-01-25 10:43:18 +0530

    exp update

--- src/components/experience/exp.js
+++ src/components/experience/exp.js
@@ -2,11 +2,7 @@ const exp = [
   {
     title: "Front End Developer @ Great Learning", 
     time: "Nov 19 - April 24",
-  },
-  {
-    title: "Freelance UI/UX Designer", 
-    time: "Jan 24 - Present",
-  },
+  }
 ]
 
 export default exp;
