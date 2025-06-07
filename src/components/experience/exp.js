bcdd38f5bae59a131c908190ea9e517c64e6e918
Harry <t.a.harry@gmail.com>
2025-06-07 14:40:38 +0530

    exp update

--- src/components/experience/exp.js
+++ src/components/experience/exp.js
@@ -1,11 +1,15 @@
 const exp = [
   {
+    title: "Software Development Engineer @ MountBlue Technologies", 
+    time: "July 2020 - October 2021",
+  },
+  {
     title: "Front End Developer @ Great Learning", 
-    time: "July 20 - April 24",
+    time: "November 2021 - April 2024",
   },
   {
     title: "Web Developer @ OTNA Infotech", 
-    time: "June 24 -",
+    time: "June 2024 -",
   },
 ]
 
